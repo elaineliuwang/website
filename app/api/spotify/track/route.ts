6383be56e9e6177aaae957a5ce7a2a47301d0a6a
Elaine Wang <elaine.liuwang@gmail.com>
2025-03-08 15:06:07 -0500

    routing

--- app/api/spotify/track/route.ts
+++ app/api/spotify/track/route.ts
@@ -10,8 +10,8 @@ export async function GET(req: Request) {
 
   try {
     // Fetch new access token (public site and local)
-    // const tokenRes = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/token`)
-    const tokenRes = await fetch(`${process.env.LOCAL_SITE_URL}/api/auth/token`)
+    const tokenRes = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/token`)
+    // const tokenRes = await fetch(`${process.env.LOCAL_SITE_URL}/api/auth/token`)
     const { access_token } = await tokenRes.json()
 
     // Fetch track details
