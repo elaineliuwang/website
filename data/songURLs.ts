acec1b2348445c61365fdebc079e8763663a5dbd
Elaine Wang <elaine.liuwang@gmail.com>
2026-01-04 12:39:16 -0600

    update music

--- data/songURLs.ts
+++ data/songURLs.ts
@@ -1,6 +1,6 @@
 export const songTrackIDs = [
-  'https://open.spotify.com/track/4Musyaro0NM5Awx8b5c627?si=29da1821e8db4602',
-  'https://open.spotify.com/track/1s6qCdD56rtIM7XMkpE8Ln?si=2e76bb9864d140b9',
-  'https://open.spotify.com/track/1F35Dmog9pekyCVnH1eQpl?si=288d0b5d070d4142',
-  'https://open.spotify.com/track/25ywKtUww26ABFd0tiGt9D?si=e8c22b6e9c2549ef'
+  'https://open.spotify.com/track/7AxXJXqJOMRqkgZxvG3r6E?si=8440f5c996044ed3',
+  'https://open.spotify.com/track/7Bam5WXYNQbbX8X4W5UmoA?si=d1524d2423d045cd',
+  'https://open.spotify.com/track/11EX5yhxr9Ihl3IN1asrfK?si=b2c0d34bbba44b39',
+  'https://open.spotify.com/track/7zxikeAavSLQVGocbK2nau?si=17e4f28a306b4f82'
 ]
